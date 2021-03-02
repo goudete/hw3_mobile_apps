c724113f3f558d941c1069ba7ee5d352b72d92cc
goudete <goudetenrique@gmail.com>
2021-03-02 17:46:39 -0600

    added linear layout with navigation buttons

--- app/src/main/java/com/example/homeworkthree/MainActivity.java
+++ app/src/main/java/com/example/homeworkthree/MainActivity.java
@@ -6,16 +6,27 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button character_btn;
+    Button location_btn;
+    Button episode_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        loadFragment(new OneFragment(), R.id.fragContainer1);
-//        loadFragment(new TwoFragment(), R.id.fragContainer2);
+        // Grabbing buttons
+        character_btn = findViewById(R.id.character_btn);
+        location_btn = findViewById(R.id.location_btn);
+        episode_btn = findViewById(R.id.episode_btn);
+
+
+//        loadFragment(new OneFragment(), R.id.fragContainer1);
+        loadFragment(new TwoFragment(), R.id.fragContainer1);
     }
 
     public void loadFragment(Fragment fragment, int id){
